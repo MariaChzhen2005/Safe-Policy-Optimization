e7eeadc389787e9c83857a9ae8c6ba53a7d74001
MariaChzhen2005 <mzcan2017china@gmail.com>
2025-11-21 12:17:47 -0500

    training

--- training_suite.sh
+++ training_suite.sh
@@ -1,14 +1,9 @@
 # PPO
-python safepo/single_agent/ppo.py --task SafetyPointPush2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
-python safepo/single_agent/ppo.py --task SafetyPointButton2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
-python safepo/single_agent/ppo.py --task SafetyPointGoal2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
+# python safepo/single_agent/ppo.py --task SafetyPointPush2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
+# python safepo/single_agent/ppo.py --task SafetyPointButton2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
+# python safepo/single_agent/ppo.py --task SafetyPointGoal2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
 
 # TRPO
 python safepo/single_agent/trpo.py --task SafetyPointPush2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
 python safepo/single_agent/trpo.py --task SafetyPointButton2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
 python safepo/single_agent/trpo.py --task SafetyPointGoal2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
-
-# PPO-Lag
-python safepo/single_agent/ppo_lag.py --task SafetyPointPush2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
-python safepo/single_agent/ppo_lag.py --task SafetyPointButton2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
-python safepo/single_agent/ppo_lag.py --task SafetyPointGoal2-v0 --device cuda --device-id 0 --experiment single_agent_exp --seed 0
--- training_suite_continued.sh
+++ training_suite_continued.sh
@@ -9,6 +9,11 @@
 # python safepo/single_agent/trpo_ae.py --task SafetyPointGoal2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
 
 # TRPO-Lag
-python safepo/single_agent/trpo_lag.py --task SafetyPointPush2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
-python safepo/single_agent/trpo_lag.py --task SafetyPointButton2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
-python safepo/single_agent/trpo_lag.py --task SafetyPointGoal2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
+# python safepo/single_agent/trpo_lag.py --task SafetyPointPush2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
+# python safepo/single_agent/trpo_lag.py --task SafetyPointButton2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
+# python safepo/single_agent/trpo_lag.py --task SafetyPointGoal2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
+
+# PPO-Lag
+python safepo/single_agent/ppo_lag.py --task SafetyPointPush2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
+python safepo/single_agent/ppo_lag.py --task SafetyPointButton2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
+python safepo/single_agent/ppo_lag.py --task SafetyPointGoal2-v0 --device cuda --device-id 1 --experiment single_agent_exp --seed 0
